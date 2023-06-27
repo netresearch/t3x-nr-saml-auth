0d5813c96318341169214a6f880ab53aaf42a31c
Gitsko <s.koschel@hotmail.de>
2023-06-27 14:12:26 +0200

    [TASK] Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.1 || 10.4.0-10.4.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
