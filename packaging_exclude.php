55ab715f4cdb72fdf7da1478d41988288a321e90
Christopher Rath <christopher.rath@netresearch.de>
2023-06-28 06:40:20 +0000

    [TASK] Add a packaging_exclude.php file

---
+++ packaging_exclude.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'directories' => [
+        '.git',
+        '.github',
+        'bin',
+        'build',
+        'public',
+        'resources\\/private\\/frontend',
+        'resources\\/private\\/libs\\/build',
+        'tailor-version-upload',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'composer.lock',
+        'docker-compose.yml',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+    ],
+];
