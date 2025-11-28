fed01c75b60bdba89144480e581f7ed6a3831c87
Sebastian Mendel <github@sebastianmendel.de>
2025-11-28 11:55:01 +0100

    refactor: update extension bootstrap for TYPO3 12+
    - Replace TYPO3_MODE constant check with TYPO3
    - Remove legacy hook registrations from ext_localconf.php
    - Remove deprecated backend module registration from ext_tables.php
    - Wrap bootstrap code in IIFE with strict types

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,33 +1,38 @@
 <?php
-defined('TYPO3_MODE') or die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Netresearch.NrSamlAuth',
-    'Authentication',
-    [
-        \Netresearch\NrSamlAuth\Controller\AuthController::class => 'login, receiveSamlResponse',
-    ],
-    [
-        \Netresearch\NrSamlAuth\Controller\AuthController::class => 'login, receiveSamlResponse',
-    ]
-);
+declare(strict_types=1);
 
+use Netresearch\NrSamlAuth\Controller\AuthController;
+use Netresearch\NrSamlAuth\Sv\AuthenticationService;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
+defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
-    'nr_saml_auth',
-    'auth',
-    \Netresearch\NrSamlAuth\Sv\AuthenticationService::class,
-    [
-        'title' => 'Authentication service',
-        'description' => 'Authentication with over a Service Provider.',
-        'subtype' => 'authUserFE,authUserBE,getUserFE',
-        'available' => true,
-        'priority' => 100,
-        'quality' => 100,
-        'className' => \Netresearch\NrSamlAuth\Sv\AuthenticationService::class
-    ]
-);
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_pre_processing'][] = \Netresearch\NrSamlAuth\Hooks\LogOffHook::class . '->logOffPreProcess';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_post_processing'][] = \Netresearch\NrSamlAuth\Hooks\LogOffHook::class . '->logOffPostProcess';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = \Netresearch\NrSamlAuth\Hooks\PostUserLookup::class . '->process';
+(static function (): void {
+    ExtensionUtility::configurePlugin(
+        'NrSamlAuth',
+        'Authentication',
+        [
+            AuthController::class => 'login, receiveSamlResponse',
+        ],
+        [
+            AuthController::class => 'login, receiveSamlResponse',
+        ]
+    );
+
+    ExtensionManagementUtility::addService(
+        'nr_saml_auth',
+        'auth',
+        AuthenticationService::class,
+        [
+            'title' => 'SAML Authentication service',
+            'description' => 'Authentication via SAML Service Provider for single sign-on (SSO)',
+            'subtype' => 'authUserFE,authUserBE,getUserFE',
+            'available' => true,
+            'priority' => 100,
+            'quality' => 100,
+            'className' => AuthenticationService::class,
+        ]
+    );
+})();
